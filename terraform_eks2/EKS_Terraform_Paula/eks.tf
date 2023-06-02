64214f48e989fd1f1b6361e74ce7f0e19b1f0f71
Paula-Encinar <pencinarsanz@gmail.com>
2023-06-02 10:00:08 +0200

    third commit

---
+++ terraform_eks2/EKS_Terraform_Paula/eks.tf
@@ -0,0 +1,44 @@
+resource "aws_iam_role" "eks-cluster" {
+  name = "eks-cluster"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "amazon-eks-cluster-policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks-cluster.name
+}
+
+resource "aws_eks_cluster" "eks" {
+  name     = "eks"
+  version  = "1.26"
+  role_arn = aws_iam_role.eks-cluster.arn
+
+  vpc_config {
+
+    endpoint_private_access = false
+    endpoint_public_access  = true
+    public_access_cidrs     = ["0.0.0.0/0"]
+
+    subnet_ids = [
+      aws_subnet.private_1.id,
+      aws_subnet.private_2.id,
+      aws_subnet.public_2.id
+    ]
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.amazon-eks-cluster-policy]
+}
